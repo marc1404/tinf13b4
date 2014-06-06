8de2ce3c7d6041cdaa165d60e20515c2ca161b29
Marc <marc.vornetran@gmail.com>
2014-06-06 12:15:12 +0200

    Bug von Patrick behoben mit 1954 statt Verlag

---
+++ marc/programmieren/src/datenstrukturen/aufgabe2/bücherverwaltung/Archiv.java
@@ -0,0 +1,54 @@
+package datenstrukturen.aufgabe2.bücherverwaltung;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class Archiv {
+
+	private File f;
+	private Collection<Buch> books = new ArrayList<Buch>();
+	
+	public Archiv(File f){
+		parseFile(f);
+	}
+	
+	private void parseFile(File f){
+		try(BufferedReader in = new BufferedReader(new FileReader(f))){
+			String line;
+			
+			while((line = in.readLine()) != null){
+				String[] split = line.split(";");
+				
+				if(split.length >= 4){
+					String autor = split[0];
+					String titel = split[1];
+					int jahr = Integer.parseInt(split[2]);
+					String verlag = split[3];
+					Buch buch = new Buch(autor, titel, jahr, verlag);
+					
+					books.add(buch);
+				}
+			}
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public void addBook(Buch buch){
+		String bookAsString = buch.getAutor() + ";" + buch.getTitle() + ";" + buch.getYear() + ";" + buch.getVerlag();
+		
+		books.add(buch);
+		
+		try{
+			Files.write(f.toPath(), bookAsString.getBytes(), StandardOpenOption.APPEND);
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+}
--- marc/programmieren/src/datenstrukturen/aufgabe2/bücherverwaltung/VerwaltungsFrame.java
+++ marc/programmieren/src/datenstrukturen/aufgabe2/bücherverwaltung/VerwaltungsFrame.java
@@ -3,6 +3,8 @@ package datenstrukturen.aufgabe2.bücherverwaltung;
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
 import javax.swing.JButton;
@@ -14,14 +16,16 @@ import javax.swing.JTextField;
 public class VerwaltungsFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
+	private Archiv archiv;
 	private JTextField autorField = new JTextField();
 	private JTextField titelField = new JTextField();
 	private JTextField jahrField = new JTextField();
 	private JTextField verlagField = new JTextField();
 	
-	public VerwaltungsFrame(){
+	public VerwaltungsFrame(Archiv archiv){
 		super("Bücherverwaltung");
 		
+		this.archiv = archiv;
 		JPanel topPanel = new JPanel(new GridLayout(4, 2, 0, 3));
 		
 		topPanel.setBorder(BorderFactory.createEmptyBorder(3, 3, 3, 3));
@@ -31,12 +35,19 @@ public class VerwaltungsFrame extends JFrame {
 		topPanel.add(titelField);
 		topPanel.add(new JLabel("Jahr"));
 		topPanel.add(jahrField);
-		topPanel.add(new JLabel("1954"));
+		topPanel.add(new JLabel("Verlag"));
 		topPanel.add(verlagField);
 		
 		JPanel centerPanel = new JPanel(new GridLayout(1, 3));
 		JButton speichernButton = new JButton("Eintrag speichern");
 
+		speichernButton.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				
+			}
+		});
+		
 		centerPanel.add(new JLabel());
 		centerPanel.add(speichernButton);
 		centerPanel.add(new JLabel());
