0bda0b7eb6e019d6fd8bf63709194ffced3133e9
Simon Schneider <simon.sch@outlook.com>
2014-06-17 23:46:19 +0200

    Dancing letters

--- simon/src/dhbw/aufgabe26/threads/dancingLetters.java
+++ simon/src/dhbw/aufgabe26/threads/dancingLetters.java
@@ -1,5 +1,16 @@
 package dhbw.aufgabe26.threads;
 
-public class dancingLetters {
+import java.util.ArrayList;
+import java.util.List;
 
+import javax.swing.JFrame;
+
+import dhbw.lib.SFrame;
+
+public class dancingLetters extends SFrame{
+	
+	public dancingLetters(){
+		
+	}
+	
 }
