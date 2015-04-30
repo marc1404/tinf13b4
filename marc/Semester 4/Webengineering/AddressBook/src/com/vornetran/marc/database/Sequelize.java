91142073586916ac66df36cc7760767a8132ee66
Marc <marc.vornetran@gmail.com>
2015-04-30 09:41:56 +0200

    First Blood

---
+++ marc/Semester 4/Webengineering/AddressBook/src/com/vornetran/marc/database/Sequelize.java
@@ -0,0 +1,35 @@
+package com.vornetran.marc.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Sequelize {
+
+	private Connection connection;
+	
+	public Sequelize(String database, String username, String password) throws Exception {
+		Class.forName("com.mysql.jdbc.Driver");
+		
+		this.connection = DriverManager.getConnection("jdbc:mysql://localhost/" + database, username, password);
+	}
+	
+	public ResultSet query(String sql) throws SQLException {
+		PreparedStatement statement = this.connection.prepareStatement(sql);
+		
+		return statement.executeQuery();
+	}
+	
+	public int update(String sql) throws SQLException {
+		PreparedStatement statement = this.connection.prepareStatement(sql);
+		
+		return statement.executeUpdate();
+	}
+	
+	public void close() throws SQLException {
+		this.connection.close();
+	}
+	
+}
---
+++ marc/Semester 4/Webengineering/Bobcat/src/com/vornetran/marc/servlets/SqrtServlet.java
@@ -0,0 +1,51 @@
+package com.vornetran.marc.servlets;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/sqrt")
+public class SqrtServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    public SqrtServlet() {
+        super();
+    }
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+        String paramX = req.getParameter("x");
+
+        OutputStreamWriter out = new OutputStreamWriter(new BufferedOutputStream(res.getOutputStream()));
+
+        out.write("<!doctype html><html><head>");
+        out.write("<title>Wurzelzieher v4.8.15</title>");
+        out.write("<link href=\"https://maxcdn.bootstrapcdn.com/bootswatch/3.3.4/cosmo/bootstrap.min.css\" rel=\"stylesheet\">");
+        out.write("</head><body>");
+        out.write("<div class=\"container\"><div class=\"row\"><div class=\"col-xs-12 col-sm-6\">");
+        out.write("<h1>Wurzelzieher v4.8.15</h1>");
+        out.write("<form>");
+        out.write("<input type=\"number\" name=\"x\" placeholder=\"Quadratzahl\" class=\"form-control\">");
+        out.write("</form>");
+
+        if(paramX != null){
+            int x = Integer.parseInt(paramX);
+
+            out.write("<hr>");
+            out.write("Die Quadratwurzel von <code>" + x + "</code> ist <code>" + Math.sqrt(x) + "</code>");
+        }
+
+        out.write("</div></div></div>");
+        out.write("</body></html>");
+        out.flush();
+    }
+
+    protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {}
+
+}
